17a2421b543ae42eb35d9fbc073d316ed8322688
Hiratake <hiratake@hiratake.xyz>
2021-04-16 00:05:16 +0900

    不要なサンプルコンポーネントを削除👋

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-export { default as JaoUiSample } from './components/jao-ui-sample.vue'
 export { default as JButton } from './components/JButton.vue'
 export { default as JImage } from './components/JImage.vue'
