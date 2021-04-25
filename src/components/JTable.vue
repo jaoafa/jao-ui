a1749428183b9f5f2dd50fb55bf3116b8669186b
Hiratake <hiratake@hiratake.xyz>
2021-04-25 18:15:40 +0900

    読み込み中表示にできるよう変更 #29

--- src/components/JTable.vue
+++ src/components/JTable.vue
@@ -6,7 +6,7 @@
     <div class="j-table__wrapper">
       <table>
         <thead
-          v-if="!hideHeader"
+          v-if="!hideHeader && !loading"
           class="j-table__header"
         >
           <tr :style="{ 'background-color': colors['gray-200'] }">
@@ -18,18 +18,37 @@
           </tr>
         </thead>
         <tbody class="j-table__body">
-          <template v-for="(item, index) in items">
-            <tr
-              :key="index"
-              :style="{
-                'background-color': (index % 2 === 0) ? null : colors['gray-50']
-              }"
-            >
-              <template v-for="column in headers">
-                <td :key="column.key">
-                  <span>{{ item[column.key] ? item[column.key] : '' }}</span>
-                </td>
-              </template>
+          <template v-if="!loading">
+            <template v-for="(item, index) in items">
+              <tr
+                :key="index"
+                :style="{
+                  'background-color': (index % 2 === 0) ? null : colors['gray-50']
+                }"
+              >
+                <template v-for="column in headers">
+                  <td :key="column.key">
+                    <span>{{ item[column.key] ? item[column.key] : '' }}</span>
+                  </td>
+                </template>
+              </tr>
+            </template>
+          </template>
+          <template v-else>
+            <tr>
+              <td>
+                <div
+                  class="j-table__progress"
+                  :style="{ color: colors['gray-300'] }"
+                >
+                  <span>読み込み中...</span>
+                  <j-progress
+                    :size="20"
+                    :stroke="2"
+                    indeterminate
+                  />
+                </div>
+              </td>
             </tr>
           </template>
         </tbody>
@@ -39,12 +58,16 @@
 </template>
 
 <script>
+import JProgress from '@/components/JProgress'
 import {
   colors,
 } from '@/utils/colors'
 
 export default {
   name: 'JTable',
+  components: {
+    JProgress,
+  },
   props: {
     headers: {
       default: () => [],
@@ -63,6 +86,10 @@ export default {
       type: Array,
     },
     hideHeader: {
+      default: false,
+      type: Boolean,
+    },
+    loading: {
       default: false,
       type: Boolean,
     },
@@ -103,5 +130,15 @@ export default {
 .j-table__wrapper {
   overflow-x: auto;
   overflow-y: hidden;
+}
+
+.j-table__progress {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  * ~ * {
+    margin-left: 8px;
+  }
 }
 </style>
