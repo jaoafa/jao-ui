aaafdf77daf7d0809e370920a0ff53038ef81a22
Hiratake <hiratake@hiratake.xyz>
2021-09-13 20:22:26 +0900

    docs: progressbarコンポーネントをstorybookに追加

---
+++ src/stories/ProgressBar.stories.js
@@ -0,0 +1,40 @@
+import JProgressBar from '@/components/JProgressBar.vue'
+
+export default {
+  title: 'Components/Progress',
+  component: JProgressBar,
+  argTypes: {
+    // props
+    absolute: {
+      description: 'コンポーネントに `position:absolute;` を適用します。',
+    },
+    color: {
+      description: '指定された色をコンポーネントに適用します。',
+    },
+    indeterminate: {
+      description: 'アニメーションを適用します。',
+    },
+    percentage: {
+      description: 'パーセントの値を指定します。',
+    },
+    stroke: {
+      description: '図形の線の太さを指定します。',
+    },
+    // events
+    // slots
+  },
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { JProgressBar },
+  template:
+    `
+    <j-progress-bar v-bind="$props" />
+    `,
+})
+
+export const Bar = Template.bind({})
+Bar.args = {
+  indeterminate: true,
+}
