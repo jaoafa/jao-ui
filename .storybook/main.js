352d9038c89c6a557b624d9f4d04f3e3d857db06
Hiratake <hiratake@hiratake.xyz>
2021-09-16 00:20:45 +0900

    chore: storybookとbabelの設定を修正
    storybook実行時に警告が出ていたためその対応

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,11 @@ const path = require('path')
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
+  ],
 
   webpackFinal: async (config) => {
     config.resolve.alias = {
