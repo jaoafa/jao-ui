65d8d0a1711f5942773e27b34dd73fce4fc554b3
Hiratake <hiratake@hiratake.xyz>
2021-09-19 23:01:07 +0900

    docs: sheetのドキュメントを追加

--- src/stories/Sheet.stories.js
+++ src/stories/Sheet.stories.js
@@ -1,9 +1,9 @@
 import JApp from '@/components/JApp.vue'
-import JCard from '@/components/JCard.vue'
+import JSheet from '@/components/JSheet.vue'
 
 export default {
-  title: 'Components/Card',
-  component: JCard,
+  title: 'Components/Sheet',
+  component: JSheet,
   argTypes: {
     // props
     color: {
@@ -14,65 +14,29 @@ export default {
     },
     height: {
       description: '指定された高さをコンポーネントに適用します。',
-      control: {
-        type: 'text',
-      },
-    },
-    href: {
-      description: 'タグを `<a>` にし、 `href` 属性を追加します。',
     },
     maxHeight: {
       description: '指定された高さの最大値をコンポーネントに適用します。',
-      control: {
-        type: 'text',
-      },
     },
     maxWidth: {
       description: '指定された幅の最大値をコンポーネントに適用します。',
-      control: {
-        type: 'text',
-      },
     },
     minHeight: {
       description: '指定された高さの最小値をコンポーネントに適用します。',
-      control: {
-        type: 'text',
-      },
     },
     minWidth: {
       description: '指定された幅の最小値をコンポーネントに適用します。',
-      control: {
-        type: 'text',
-      },
-    },
-    nuxt: {
-      description: 'タグを `<nuxt-link>` にします。',
     },
     tag: {
       description: '指定されたタグをコンポーネントに適用します。',
     },
-    target: {
-      description:
-        '指定された値を `target` 属性としてコンポーネントに追加します。',
-    },
     tile: {
       description: 'コンポーネントの角を角丸にしないようにします。',
     },
-    to: {
-      description:
-        'コンポーネントを `<router-link>` にし、 `to` Prop を適用します。',
-    },
     width: {
       description: '指定された幅をコンポーネントに適用します。',
-      control: {
-        type: 'text',
-      },
     },
     // events
-    click: {
-      description: 'コンポーネントがクリックされたときに発生するイベントです。',
-      action: 'click',
-    },
     // slots
     default: {
       description: 'コンポーネントの中に表示するコンテンツです。',
@@ -87,15 +51,17 @@ export default {
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { JApp, JCard },
+  components: { JApp, JSheet },
   template: `
     <j-app no-stretch>
-      <j-card @click="click" v-bind="$props">
-        Card Content
-      </j-card>
+      <j-sheet v-bind="$props" />
     </j-app>
-    `,
+  `,
 })
 
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {
+  color: 'primary',
+  width: '320px',
+  height: '160px',
+}
