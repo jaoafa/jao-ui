cb2a91eb6f132d1a6ec237898a95ca39cd0a7894
Hiratake <hiratake@hiratake.xyz>
2022-01-27 20:26:38 +0900

    chore: アプリケーションのメインコンポーネントをsetup scriptで書き換え

--- src/components/JApp/JAppMain.vue
+++ src/components/JApp/JAppMain.vue
@@ -1,46 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { ComponentTagClasses } from '@/types'
+
+// props
+type Props = {
+  /** 指定されたタグをコンポーネントに適用します */
+  tag?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  tag: 'main',
+})
+
+// class
+const classes = computed(
+  (): ComponentTagClasses<'j-app-main'> => ({
+    'j-app-main': true,
+  })
+)
+</script>
+
 <template>
-  <component :is="tag" :class="classes" :style="styles">
+  <component :is="props.tag" :class="classes">
     <slot />
   </component>
 </template>
-
-<script lang="ts">
-import { computed, defineComponent, onMounted, ref } from 'vue'
-
-export default defineComponent({
-  name: 'JAppMain',
-
-  props: {
-    /** 指定されたタグをコンポーネントに適用します */
-    tag: {
-      type: String,
-      default: 'main',
-    },
-  },
-
-  setup() {
-    const classes = computed((): { [key: string]: boolean } => ({
-      'j-app-main': true,
-    }))
-
-    const styles = computed((): { [key: string]: string } => ({
-      padding: `${paddingTop.value}px 0 0 0`,
-    }))
-
-    const paddingTop = ref(0)
-
-    onMounted(() => {
-      const element = document.querySelector(
-        '.j-app > .j-app-header--fixed'
-      ) as HTMLElement
-      if (element) {
-        paddingTop.value = element.offsetHeight
-      }
-    })
-    return { classes, styles }
-  },
-})
-</script>
 
 <style lang="scss">
 @use 'src/styles/includes' as *;
