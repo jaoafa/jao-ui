9eb43b1d5d29cd363eb1647133cd5e61296a02f8
Hiratake <hiratake@hiratake.xyz>
2022-01-27 20:30:12 +0900

    chore: アプリケーションのサイドバーコンポーネントをsetup scriptで書き換え

--- src/components/JApp/JAppSidebar.vue
+++ src/components/JApp/JAppSidebar.vue
@@ -1,31 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { ComponentTagClasses } from '@/types'
+
+// props
+type Props = {
+  /** 指定されたタグをコンポーネントに適用します */
+  tag: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  tag: 'aside',
+})
+
+// class
+const classes = computed(
+  (): ComponentTagClasses<'j-app-sidebar'> => ({
+    'j-app-sidebar': true,
+  })
+)
+</script>
+
 <template>
-  <component :is="tag" :class="classes">
+  <component :is="props.tag" :class="classes">
     <slot />
   </component>
 </template>
-
-<script lang="ts">
-import { computed, defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'JAppSidebar',
-
-  props: {
-    /** 指定されたタグをコンポーネントに適用します */
-    tag: {
-      type: String,
-      default: 'aside',
-    },
-  },
-
-  setup() {
-    const classes = computed((): { [key: string]: boolean } => ({
-      'j-app-sidebar': true,
-    }))
-    return { classes }
-  },
-})
-</script>
 
 <style lang="scss">
 @use 'src/styles/includes' as *;
